ddf90b472c696adeb9b804e9653869580df8dad3
gnusik533 <asgargion14@gmail.com>
2024-07-12 13:27:42 +0400

    Only main page.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Http\Controllers\LotteryController;
-use App\Http\Controllers\PrizeController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
     $user = \App\Models\User::first();
@@ -19,43 +15,3 @@ Route::get('/', function () {
 
     return view('index');
 })->name('home');
-
-Route::get('/redeem/{qr}', [PrizeController::class, 'redeem'])
-    ->name('redeem');
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/lotteries', function () {
-        return Inertia::render('Lottery');
-    })->name('lotteries');
-
-    Route::get('/lotteries/index', [LotteryController::class, 'index'])
-        ->name('lotteries.index');
-
-    Route::prefix('/lottery')->name('lottery.')->group(function () {
-        Route::get('/create', [LotteryController::class, 'create'])
-            ->name('create');
-
-        Route::post('/lottery/create', [LotteryController::class, 'store'])
-            ->name('store');
-
-        Route::get('/show/{lottery}', [LotteryController::class, 'show'])
-            ->name('show');
-
-        Route::post('/draw-all/{lottery}', [LotteryController::class, 'draw'])
-            ->name('draw.all');
-
-        Route::post('/update/{lottery}', [LotteryController::class, 'update'])
-            ->name('update');
-
-        Route::post('/prize/update/{prize}', [PrizeController::class, 'update'])
-            ->name('prize.update');
-    });
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
